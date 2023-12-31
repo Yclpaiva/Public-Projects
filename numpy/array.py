bac1531b777780c3a87442fe919d9bac23005c19
Yuri Paiva <yclpaiva@gmail.com>
2023-12-30 22:54:04 -0300

    teste

---
+++ numpy/array.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+n = np.array(range(101))
+m = np.empty([5,3])
+print(m)
+identity_matrix = np.eye(9)
+print('identity matrix',identity_matrix)
+
+arange = np.arange(2,100,2)
+print(arange)
---
+++ random/list comprehension/teste.py
@@ -0,0 +1,2 @@
+n = list(x*2 for x in range(101) if x % 2 == 0)
+print(n)
