1612e8722be6803be28dd99a4028459319191407
Yuri Paiva <149324858+Yclpaiva@users.noreply.github.com>
2024-01-03 09:17:01 -0300

    Create python numbers.py

---
+++ random/python numbers.py
@@ -0,0 +1,26 @@
+def float_scientific():
+    x = 35e3
+    y = 12E4
+    z = -87.7e14
+    print(type(x),x)
+    print(type(y),y)
+    print(type(z),z)
+    
+    
+def typeconversion():
+    x = 1
+    y = 2.8
+    z = 1j
+    a = float(x)
+    b = int(y)
+    c = complex(x)
+    print(a)
+    print(b)
+    print(c)
+    print(type(a))
+    print(type(b))
+    print(type(c))
+    
+    
+    
+float_scientific()
