3b991da6acf2d55caa53602f10c42c602dcf4142
Yuri Paiva <149324858+Yclpaiva@users.noreply.github.com>
2024-01-03 09:11:23 -0300

    Create data types.py

---
+++ random/data types.py
@@ -0,0 +1,15 @@
+string = "Hello World"
+inteiro = 20
+float_n = 20.5 #números decimais
+complexo = 13j #números complexos
+lista = ["apple", "banana", "cherry"]
+tupla = ("apple", "banana", "cherry")
+range_ = range(6)
+dicio = {"name" : "John", "age" : 36}
+set_ = {"apple", "banana", "cherry", 'cherry'} #não tem ordem, os itens aparecem aleatoriamente, ele também remove duplicas
+frozenset_ = frozenset({"apple", "banana", "cherry", 'cherry'})
+boolean = True
+bytes_ = b"Hello"
+bytearray_ = bytearray(3)
+memoryview_ = memoryview(bytearray_)
+noneType = None
