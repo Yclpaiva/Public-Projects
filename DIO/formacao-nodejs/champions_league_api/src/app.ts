f6c1ad9f21f27befb5d5b274e3546c1c1123d6bc
yclpaiva <yclpaiva@gmail.com>
2024-05-30 16:41:53 -0300

    a

---
+++ DIO/formacao-nodejs/champions_league_api/src/app.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import router from "./routes";
+import cors from "cors";
+
+function createApp() {
+  const app = express();
+
+  app.use(express.json());
+  app.use("/api", router);
+
+  const corsOptions = {
+    origin: "*",
+    methods: ["GET", "UPDATE"],
+  };
+
+  app.use(cors(corsOptions));
+  return app;
+}
+
+export default createApp;
